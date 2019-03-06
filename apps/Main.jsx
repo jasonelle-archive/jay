e86f4ad4b9639f7db39f2842b781a5bd0d4870ef
Camilo <camilo@ninjas.cl>
2019-03-06 02:00:43 -0300

    improved comments

--- apps/Main.jsx
+++ apps/Main.jsx
@@ -2,7 +2,7 @@ import { render, Jason } from 'jason';
 
 // This is the bare minimum needed in order to render an app.
 // It's important to return the rendered component
-// in order to transform the result to json.
+// to transform the result into JSON.
 // Only call this function in the JSX that will be
-// passed to the index.js file in root.
+// passed to the index.js file in root directory.
 export default render(<Jason />);
