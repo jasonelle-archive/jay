3b290902d8a42028f89941821dc46bbf472f8e32
Camilo <camilo@ninjas.cl>
2019-03-08 18:20:19 -0300

    added $lambda options validations

--- jason/Common/Action/Types/Lambda/Options.jsx
+++ jason/Common/Action/Types/Lambda/Options.jsx
@@ -1,10 +1,20 @@
 import Options from '../../../Options';
 import validations from '../../../../validations';
 
+const context = '$lambda.options';
+
 const LambdaOptions = ({ name, children }) => {
   if (!name) {
-    validations.errors.required('name', '$lambda.options');
+    validations.errors.required('name', context);
   }
+
+  if (!children) {
+    validations.errors.required('options', context);
+  }
+
+  const validChildren = ['options'];
+  validations.children.are.valid(children, validChildren, context);
+
   return <Options name={name}>{children}</Options>;
 };
 
